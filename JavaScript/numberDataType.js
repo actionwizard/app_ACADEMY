4487643fc5e2825e25010d0d5cee6315d329ceb3
Anish Kumar <anishykumar42@gmail.com>
2023-08-25 12:38:05 +0530

    Created the repo and started learning JS

---
+++ JavaScript/numberDataType.js
@@ -0,0 +1,23 @@
+/*
+NOTES ::==>> The Number type is used to represent integer and decimal values
+*/
+
+// Example:
+console.log(4 + 2.5); // 6.5
+
+// Write 5 more expressions below:
+
+//Expression 1
+console.log(16 - 9.5); //6.5
+
+//Expression 2
+console.log(1.3 * 5); //6.5
+
+//Expression 3
+console.log(13 / 2); //6.5
+
+//Expression 4
+console.log(6 + 0.5); //6.5
+
+//Expression 5
+console.log(6.5 % 7); //6.5
